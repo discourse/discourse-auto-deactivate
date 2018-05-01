0053f41485176f2700fc25fa801ff81c580f9641
Blake Erickson <o.blakeerickson@gmail.com>
2018-05-01 09:45:40 -0600

    Created job that runs daily to deactivate inactive users.

---
+++ plugin.rb
@@ -0,0 +1,50 @@
+# name: discourse-auto-deactivate
+# about: Automatically deactivates inactive users so that they need to recomfirm their email in order to login in again
+# version: 0.0.1
+# authors: David Taylor, Blake Erickson
+# url: https://github.com/discourse/discourse-auto-deactivate
+
+enabled_site_setting :auto_deactivate_enabled
+
+PLUGIN_NAME ||= 'discourse_auto_deactivate'.freeze
+
+after_initialize do
+  module ::DiscourseAutoDeactivate
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseAutoDeactivate
+    end
+  end
+
+
+
+  module ::Jobs
+    class AutoDeactivateUsers < Jobs::Scheduled
+      every 1.day
+
+      def execute(args)
+        return if !SiteSetting.auto_deactivate_enabled?
+
+        auto_deactivate_days = SiteSetting.auto_deactivate_after_days.days.ago
+        to_deactivate = User.where("last_seen_at IS NULL OR last_seen_at < ? AND created_at < ?", auto_deactivate_days, auto_deactivate_days)
+                         .where('active = ?', true)
+                         .real
+
+        for user in to_deactivate do
+          safe_groups = SiteSetting.auto_deactivate_safe_groups
+          user_is_safe = user.groups.any?{|g| safe_groups.include? g.name}
+
+          if not user_is_safe then
+            user.active = false
+            deactivate_reason = I18n.t("discourse_auto_deactivate.deactivate-reason")
+
+            if user.save
+              StaffActionLogger.new(Discourse.system_user).log_user_deactivate(user, deactivate_reason)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
